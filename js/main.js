5b63db7ab2f16f07ed5d93a751471cc686a770ae
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2023-01-09 16:31:38 -0300

    robotron-2000

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-const Controles = document.querySelectorAll('.controle-ajuste');
+const Controles = document.querySelectorAll('[data-controle]');
 
 function manipularDom(operacao, div) {
-  const contador = div.querySelector('.controle-contador');
+  const contador = div.querySelector('[data-contador]');
   operacao === "+" ? contador.value = parseInt(contador.value) + 1 : contador.value = parseInt(contador.value) - 1
 };
 
 Controles.forEach(elemento => {
   elemento.addEventListener('click', evento => {
-    manipularDom(evento.target.textContent, evento.target.parentNode)
+    manipularDom(evento.target.dataset.controle, evento.target.parentNode)
   })
 });
